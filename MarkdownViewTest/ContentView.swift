ee120f56bf7dc37129e029b07740e6dff7ae4120
Christophe Guegan <cguegan@gmail.com>
2025-07-05 08:00:55 +0200

    Initial Commit

---
+++ MarkdownViewTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownViewTest
+//
+//  Created by Christophe Guégan on 05/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownViewTest/MarkdownViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewTestApp.swift
+//  MarkdownViewTest
+//
+//  Created by Christophe Guégan on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
